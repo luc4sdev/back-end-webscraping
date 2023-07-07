2bb21ef5bac15540de0863f8aeecb7728b6f0b2b
lucasLivefarm <lucasgamesdark@gmail.com>
2023-07-07 17:10:32 -0300

    cors error

--- api/index.js
+++ api/index.js
@@ -13,11 +13,19 @@ const app = express();
 const route = Router()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({
-  origin: "https://front-end-webscraping-git-main-luc4sdev.vercel.app/?vercelToolbarCode=GwFbJHqgkJLM57p",
-  headers: ["Content-Type"],
-  credentials: false,
-}));
+
+const whitelist = ["http://localhost:5173"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 
 route.get('/api/data-forex', async (req, res) => {
