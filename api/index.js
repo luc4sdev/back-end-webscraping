e303ef99d186f78097e7061b88cc2a71439d802e
lucasLivefarm <lucasgamesdark@gmail.com>
2023-07-08 07:15:30 -0300

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import 'dotenv/config.js';
 import express from 'express';
-
+import cors from 'cors';
 import { getForexFactoryData } from './forex.js';
 import { getInvestingData } from './investing.js';
 import { getFinancialJuiceData } from './financial.js';
@@ -11,22 +11,33 @@ const route = Router();
 
 app.use(express.json());
 
+var allowlist = ['https://front-v0x8.onrender.com/', 'http://localhost:5173']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
-route.post('/api/data-forex', async (req, res) => {
+
+route.post('/api/data-forex', cors(corsOptionsDelegate), async (req, res) => {
   const { tag } = req.body;
   console.log(tag);
   const forexData = await getForexFactoryData(tag);
   res.json(forexData);
 });
 
-route.post('/api/data-financial', async (req, res) => {
+route.post('/api/data-financial',cors(corsOptionsDelegate), async (req, res) => {
   const { tag } = req.body;
   console.log(tag);
   const financialData = await getFinancialJuiceData(tag);
   res.json(financialData);
 });
 
-route.post('/api/data-investing', async (req, res) => {
+route.post('/api/data-investing',cors(corsOptionsDelegate), async (req, res) => {
   const { tag } = req.body;
   console.log(tag);
   const investingData = await getInvestingData(tag);
