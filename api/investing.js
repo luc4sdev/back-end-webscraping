a605c355e76cfe512e4ce5dca03d724edd7c2434
lucasLivefarm <lucasgamesdark@gmail.com>
2023-07-08 00:36:51 -0300

    v final 2

--- api/investing.js
+++ api/investing.js
@@ -6,14 +6,14 @@ import puppeteer from 'puppeteer';
 
     const dataInvesting = {
       name: 'investing',
-      url: 'https://br.investing.com/economic-calendar/',
+      url: 'https://za.investing.com/economic-calendar/?endDate=1688774400&startDate=1688688000&timeFrame=yesterday',
       mainTag: tag,
-      timeTag: 'time',
-      currecyTag: 'flagCur',
-      eventTag: 'event',
-      actualTag: 'act ',
-      forecastTag: 'fore',
-      previousTag: 'prev',
+      timeTag: 'col-time',
+      currecyTag: 'col-country',
+      eventTag: 'col-event',
+      actualTag: 'col-actual ',
+      forecastTag: 'col-forecast',
+      previousTag: 'col-previous',
       time: '',
       currency: '',
       event: '',
@@ -46,39 +46,68 @@ import puppeteer from 'puppeteer';
   
 
       const time = await page.$eval(
-        `tr${tag} td.${dataInvesting.timeTag}`,
+        `tr${tag} td.${dataInvesting.timeTag} span`,
         (element) => element.textContent.trim().replace(/^\s+|\n/g, '')
       );
-      const currency = await page.$eval(
-        `tr${tag} td.${dataInvesting.currecyTag}`,
-        (element) => element.textContent.trim().replace(/^\s+|\n/g, '')
-      );
-      const event = await page.$eval(
-        `tr${tag} td.${dataInvesting.eventTag} a`,
-        (element) => element.textContent
-      );
-      const actual = await page.$eval(
-        `tr${tag} td.${dataInvesting.actualTag}`,
-        (element) => element.textContent
-      );
-      const forecast = await page.$eval(
-        `tr${tag} td.${dataInvesting.forecastTag}`,
-        (element) => element.textContent
-      );
-      const previous = await page.$eval(
-        `tr${tag} td.${dataInvesting.previousTag} span`,
-        (element) => element.textContent
-      );
+
+      const spans = await page.$$(`tr${tag} td.${dataInvesting.currecyTag} div span`);
+
+      if (spans.length >= 2) {
+        const segundaDiv = spans[1];
   
+  
+        if (segundaDiv) {
+            const textoCurrency = await page.evaluate((el) => el.textContent, segundaDiv);
+            const partes = textoCurrency.split(' ');
+            const p1 = partes[0];
+            dataInvesting.currency = p1;
+          
+        }
+      }
+  
+      const event = await page.$eval(
+        `tr${tag} td.${dataInvesting.eventTag} span a`,
+        (element) => element.textContent
+      );
+
+      const divs = await page.$$(`tr${tag} td.${dataInvesting.actualTag} span`);
+
+      if (divs.length >= 2) {
+        const segundaDiv = divs[1];
+  
+  
+        if (segundaDiv) {
+            const textActual = await page.evaluate((el) => el.textContent, segundaDiv);
+            const partes = textActual.split(' ');
+            const p1 = partes[0];
+            dataInvesting.actual = p1;
+          
+        }
+      }
+
+
+      const divs2 = await page.$$(`tr${tag} td.${dataInvesting.forecastTag} span`);
+
+      if (divs2.length >= 2) {
+        const segundaDiv = divs2[1];
+  
+  
+        if (segundaDiv) {
+            const textForecast = await page.evaluate((el) => el.textContent, segundaDiv);
+            const partes = textForecast.split(' ');
+            const p1 = partes[0];
+            dataInvesting.forecast = p1;
+          
+        }
+      }
+      const previous = await page.$eval(
+        `tr${tag} td.${dataInvesting.previousTag} div span`,
+        (element) => element.textContent
+      );
   
 
-  
       dataInvesting.time = time;
-      dataInvesting.time = time;
-      dataInvesting.currency = currency;
       dataInvesting.event = event;
-      dataInvesting.actual = actual;
-      dataInvesting.forecast = forecast;
       dataInvesting.previous = previous;
 
       await browser.close();
